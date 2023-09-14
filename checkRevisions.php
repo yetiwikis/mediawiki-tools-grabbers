6a05502d37dc91d2ec0e9b74e261c4ca880f6694
Jayden Bailey <jaydenkieran@gmail.com>
2023-09-14 04:44:20 +0100

    checkRevisions.php: Re-acquire revisions with mismatching SHAs and len=0

--- checkRevisions.php
+++ checkRevisions.php
@@ -12,12 +12,14 @@
  * @date 10 September 2023
  */
 
-use MediaWiki\MediaWikiServices;
+use MediaWiki\Revision\RevisionRecord;
+use MediaWiki\Revision\MutableRevisionRecord;
 use MediaWiki\Revision\RevisionAccessException;
+use MediaWiki\Revision\SlotRecord;
 
-require_once 'includes/ExternalWikiGrabber.php';
+require_once 'includes/TextGrabber.php';
 
-class CheckRevisions extends ExternalWikiGrabber {
+class CheckRevisions extends TextGrabber {
 
 	/**
 	 * The report interval
@@ -46,6 +48,13 @@ class CheckRevisions extends ExternalWikiGrabber {
 	 * @var int
 	 */
 	protected $mismatchCount = 0;
+
+	/**
+	 * The current replaced revisions count
+	 *
+	 * @var int
+	 */
+	protected $replacedCount = 0;
 
 	public function __construct() {
 		parent::__construct();
@@ -78,7 +87,7 @@ class CheckRevisions extends ExternalWikiGrabber {
 
 			foreach ( $result['query']['allrevisions'] as $page ) {
 				foreach ( $page['revisions'] as $rev ) {
-					$this->processRevision( $rev );
+					$this->handleRevision( $rev );
 					$this->revCount++;
 					if ( $this->revCount >= $checkpoint ) {
 						$this->output( "{$this->revCount} revisions processed.\n" );
@@ -94,12 +103,11 @@ class CheckRevisions extends ExternalWikiGrabber {
 			}
 		} while ( $more );
 
-		$this->output( "Done.\n\n$this->revCount revisions checked.\n$this->missingCount revisions missing.\n$this->mismatchCount hash mismatches.\n" );
+		$this->output( "Done.\n\n$this->revCount revisions checked.\n$this->missingCount revisions missing.\n$this->mismatchCount hash mismatches.\n$this->replacedCount revisions replaced.\n" );
 	}
 
-	public function processRevision( $remoteRev ) {
-		$revStore = MediaWikiServices::getInstance()->getRevisionStore();
-		$rev = $revStore->getRevisionById( $remoteRev['revid'] );
+	public function handleRevision( $remoteRev ) {
+		$rev = $this->revisionStore->getRevisionById( $remoteRev['revid'] );
 
 		if ( is_null( $rev ) ) {
 			// The revision is missing from our database.
@@ -112,9 +120,57 @@ class CheckRevisions extends ExternalWikiGrabber {
 					// The checksum of our revision and the remote revision doesn't match.
 					$this->output( "Bad revision (hash): {$remoteRev['revid']} (ours: ${sha} | theirs: ${remoteRev['sha1']})\n" );
 					$this->mismatchCount++;
+
+					// Check whether our revision is an empty revision, because sites like Fandom often have empty
+					// revision content on a random number of pages inside their XML dumps.
+					if ( $rev->getSize() === 0 ) {
+						// If it is empty, then replace it with the content from the remote wiki.
+						// We fetch the revision text again separately here, because getting "content" is expensive
+						// to do all the time in the original API call.
+						$params = [
+							'prop' => 'revisions',
+							'rvprop' => 'ids|timestamp|sha1|content|contentmodel',
+							'revids' => $remoteRev['revid']
+						];
+						$result = $this->bot->query( $params );
+
+						if ( empty( $result['query']['pages'] ) ) {
+							$this->error( "Could not replace revision {$remoteRev['revid']} with remote wiki: bad API call." );
+							return;
+						}
+
+						$remoteRev = $result['query']['pages'][array_key_first( $result['query']['pages'] )]['revisions'][0];
+						$content = ContentHandler::makeContent( $remoteRev['*'], null, $remoteRev['contentmodel'], $remoteRev['contentformat'] );
+
+						// TODO: do it in a better way than the next lines of code, which feel jank...
+						$updatedRev = MutableRevisionRecord::newUpdatedRevisionRecord( $rev, [] );
+						$updatedRev->setContent( SlotRecord::MAIN, $content );
+						$updatedRev->setTimestamp( $rev->getTimestamp() );
+						$updatedRev->setMinorEdit( $rev->isMinor() );
+						$updatedRev->setComment( $rev->getComment() );
+						$updatedRev->setVisibility( $rev->getVisibility() );
+
+						// Delete our version of the revision and save the new version.
+						$this->dbw->delete(
+							'revision',
+							[ 'rev_id' => $remoteRev['revid'] ],
+							__METHOD__
+						);
+						$this->dbw->delete(
+							'slots',
+							[ 'slot_revision_id' => $remoteRev['revid'] ],
+							__METHOD__
+						);
+
+						$this->revisionStore->insertRevisionOn( $updatedRev, $this->dbw );
+
+						$this->output("Replaced revision {$remoteRev['revid']} with content from remote wiki\n");
+						$this->replacedCount++;
+					}
 				}
 			} catch ( RevisionAccessException $e ) {
 				// SHA doesn't exist, or something went wrong, so let's just play safe and do nothing.
+				$this->output( "Problem processing revision {$remoteRev['revid']}: {$e->getMessage()}\n" );
 			}
 		}
 	}
